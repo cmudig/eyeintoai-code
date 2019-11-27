43f41eff9f834ae49ef9b90c300a35ee1f901331
Kazi Jawad <kazi_Jawad@outlook.com>
2019-11-26 21:50:10 -0500

    Lint root react files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,72 +1,67 @@
 import React, { Component } from 'react';
-import { HashRouter, Route, Link } from "react-router-dom";
+import { HashRouter, Route, Link } from 'react-router-dom';
+
 import './guessai/index.scss';
+import cmuLogo from './image/CMU_Logo.png';
+import GuessAI from './guessai/guessai.js';
+import Home from './home.js';
+import GAIHome from './guessAIHome.js';
 
-import cmuLogo from './image/CMU_Logo.png'
-import GuessAI from './guessai/guessai.js'
-import Home from './home.js'
-import GAIHome from './guessAIHome.js'
-import AIQuiz from './aiQuiz/aiQuiz.js'
-
-const profiles = ["fas fa-otter", "fas fa-hippo", "fas fa-dog", "fas fa-crow", "fas fa-horse", "fas fa-frog", "fas fa-fish", "fas fa-dragon", "fas fa-dove", "fas fa-spider", "fas fa-cat"]
-
+const profiles = ['fas fa-otter', 'fas fa-hippo', 'fas fa-dog', 'fas fa-crow', 'fas fa-horse', 'fas fa-frog', 'fas fa-fish', 'fas fa-dragon', 'fas fa-dove', 'fas fa-spider', 'fas fa-cat'];
 
 /* global gapi */
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isSignedIn: false,
-      players: [],
-      gameClass: [" ", " "],
-      playerProfile: null,
-    }
-  }
+  state = {
+    isSignedIn: false,
+    players: [],
+    gameClass: [' ', ' '],
+    playerProfile: null,
+  };
 
   componentDidMount() {
     window.addEventListener('beforeunload', this.handleLeavePage);
     window.gapi.load('auth2', () => {
       this.auth2 = gapi.auth2.init({
         client_id: '423634020815-9pu8kc2gfh3s7rejq6d7k1nmcaqn70d4.apps.googleusercontent.com',
-        scope: 'profile'
-      })
-    })
+        scope: 'profile',
+      });
+    });
     window.gapi.signin2.render('g-signin2', {
       'theme': 'dark',
       'onsuccess': this.onSuccess.bind(this),
-    });  
+    });
     this.selectProfile();
   }
 
   componentWillUnmount() {
     window.removeEventListener('beforeunload', this.handleAppStateChange);
-   
   }
-  
-  handleLeavePage = (e) => {
+
+  handleLeavePage = () => {
     this.signOut();
-    this.state.isSignedIn = false;
-    return "leave";
+    this.setState({ isSignedIn: false });
+    return 'leave';
   }
-  
-  selectProfile(){
-    //randomly select players' profile
-    let ranNum = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  selectProfile() {
+    // randomly select players' profile
+    const ranNum = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     let ran1, ran2, ranTemp;
     for (let i = 0; i < 10; i++) {
       ran1 = Math.floor(Math.random() * 11);
       ran2 = Math.floor(Math.random() * 11);
- 
+
       ranTemp = ranNum[ran1];
-      ranNum[ran1] = ranNum[ran2]
+      ranNum[ran1] = ranNum[ran2];
       ranNum[ran2] = ranTemp;
     }
-    let players = [];
+    const players = [];
     players.push(this.constructPlayer(profiles[ranNum[0]]));
     players.push(this.constructPlayer(profiles[ranNum[1]]));
     players.push(this.constructPlayer(profiles[ranNum[2]]));
-    this.setState({players: players});
+    this.setState({ players: players });
   }
+
   constructPlayer(name) {
     return {
       img: <i className={name}></i>,
@@ -74,55 +69,65 @@ class App extends Component {
       score: 0,
     };
   }
-  //change the url depending on the pages
-  setMenu(i){
-    let menuClass = [" ", " "];
-    menuClass[i] = "active";
-    this.setState({gameClass: menuClass})
 
+  // Change the url depending on the pages
+  setMenu(i) {
+    const menuClass = [' ', ' '];
+    menuClass[i] = 'active';
+    this.setState({ gameClass: menuClass });
   }
 
   onSuccess(googleUser) {
     if (this.state.isSignedIn) {
       this.signOut();
     } else {
-      let playerProfile =  googleUser.getBasicProfile();
-      let players = this.state.players;
-      
+      const playerProfile = googleUser.getBasicProfile();
+      const players = this.state.players;
+
       players[0].img = <i className={players[0].name}><img src={playerProfile.getImageUrl()}></img></i>;
       players[0].name = playerProfile.getGivenName();
-      this.setState({players: players, playerProfile: playerProfile});
+      this.setState({ players: players, playerProfile: playerProfile });
     }
-    this.state.isSignedIn = !this.state.isSignedIn;
+    this.setState({ isSignedIn: !this.state.isSignedIn });
   }
 
   signOut() {
-    this.auth2.signOut().then(function () {
-    });
+    this.auth2.signOut();
     this.auth2.disconnect();
   }
-  
+
   render() {
-    return (<HashRouter basename = "/">
-      <div className="App" style={{ width: "100%", height: "100%", position:"relative"}} key="main">
-        <div className="header" >
-          <div id="cmu">
-            <img src = {cmuLogo} alt="CMU logo" /></div>
-            
-            <Link to="/home/" className="title" onClick={(ev)=> {this.setState({ gameClass: [" ", " "]}); }}>Interpretable Machine Learning Research Project</Link>
-            <div className = "menuBar">
-           
-            <Link to="/guessai/" className={"menu " + this.state.gameClass[0]} key="menu0" onClick={(ev)=> {this.setState({ gameClass: ["active", " "]}); }}>Guess AI</Link>
-             <div id="g-signin2" onClick={(env) => {this.signOut.bind(this)}}></div>
+    return (
+      <HashRouter basename = "/">
+        <div className="App" style={{ width: '100%', height: '100%', position:'relative' }} key="main">
+          <div className="header" >
+            <div id="cmu">
+              <img src = {cmuLogo} alt="CMU logo" />
+            </div>
+            <Link
+              to="/home/"
+              className="title"
+              onClick={()=> { this.setState({ gameClass: [' ', ' '] }); }}
+            >
+              Interpretable Machine Learning Research Project
+            </Link>
+            <div className="menuBar">
+              <Link
+                to="/guessai/"
+                className={'menu ' + this.state.gameClass[0]}
+                key="menu0"
+                onClick={()=> { this.setState({ gameClass: ['active', ' '] }); }}
+              >
+                Guess AI
+              </Link>
+              <div id="g-signin2" onClick={() => { this.signOut.bind(this) ; }}></div>
             </div>
           </div>
-         
-            <Route path ="/" exact render={props => <Home setMenu = {this.setMenu.bind(this)} />} />
-            <Route path ="/home/" render={props => <Home setMenu = {this.setMenu.bind(this)} />} />
-            <Route path = "/guessai/" render={props => <GAIHome  setMenu = {this.setMenu.bind(this)}/>} />
-            <Route path = "/guessai-play/" render = {props => <GuessAI key = "guessAI" players = {this.state.players} setMenu = {this.setMenu.bind(this)} />}  />
-         
-      </div>
+          <Route path ="/" exact render={() => <Home setMenu = {this.setMenu.bind(this)} />} />
+          <Route path ="/home/" render={() => <Home setMenu = {this.setMenu.bind(this)} />} />
+          <Route path = "/guessai/" render={() => <GAIHome setMenu = {this.setMenu.bind(this)}/>} />
+          <Route path = "/guessai-play/" render = {() => <GuessAI key = "guessAI" players = {this.state.players} setMenu = {this.setMenu.bind(this)} />} />
+        </div>
       </HashRouter>
     );
   }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -10,5 +11,3 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-
-  
