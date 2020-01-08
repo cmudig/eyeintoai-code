e99fdd85279809cff36d5693b4754eb5035c9e15
Kazi Jawad <kazi_Jawad@outlook.com>
2020-01-08 13:59:12 -0500

    Start redesign

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
-import { HashRouter, Route, Link } from "react-router-dom";
-import './guessai/index.scss';
-import cmuLogo from './image/CMU_Logo.png'
-import GuessAI from './guessai/guessai.js'
-import Home from './home.js'
-import GAIHome from './guessAIHome.js'
+import { HashRouter, Route, Link } from 'react-router-dom';
 import * as firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import dotenv from 'dotenv';
 
-require('dotenv').config()
-require("firebase/firestore");
-require("firebase/auth");
+import Landing from './Landing/Landing';
+import GuessAI from './guessai/guessai.js';
 
-const profiles = ["fas fa-otter", "fas fa-hippo", "fas fa-dog", "fas fa-crow", "fas fa-horse", "fas fa-frog", "fas fa-fish", "fas fa-dragon", "fas fa-dove", "fas fa-spider", "fas fa-cat"]
+import './guessai/index.scss';
+import cmuLogo from './image/CMU_Logo.png';
+
+dotenv.config();
+const profiles = ['fas fa-otter', 'fas fa-hippo', 'fas fa-dog', 'fas fa-crow', 'fas fa-horse', 'fas fa-frog', 'fas fa-fish', 'fas fa-dragon', 'fas fa-dove', 'fas fa-spider', 'fas fa-cat'];
+
 /* global gapi */
 class App extends Component {
   constructor(props) {
@@ -19,45 +21,45 @@ class App extends Component {
     this.state = {
       isSignedIn: false,
       players: [],
-      gameClass: [" ", " "],
-    }
+      gameClass: [' ', ' '],
+    };
     this.googleUser = null;
-    this.uid = "";
+    this.uid = '';
     this.db = null;
     this.timestamp = null;
   }
 
-  componentDidMount() {    
+  componentDidMount() {
     window.addEventListener('beforeunload', this.handleLeavePage.bind(this));
     window.gapi.load('auth2', () => {
       gapi.auth2.init({
         client_id: '423634020815-9pu8kc2gfh3s7rejq6d7k1nmcaqn70d4.apps.googleusercontent.com',
-        scope: 'profile'
+        scope: 'profile',
       }).then(() => {
         this.auth = window.gapi.auth2.getAuthInstance();
         this.handleAuthChange();
         this.auth.isSignedIn.listen(this.handleAuthChange);
       });
-    })
+    });
     if (!this.db) {
-      let firebaseConfig = {
-        apiKey: process.env.REACT_APP_apiKey, 
-        authDomain: process.env.REACT_APP_authDomain, 
+      const firebaseConfig = {
+        apiKey: process.env.REACT_APP_apiKey,
+        authDomain: process.env.REACT_APP_authDomain,
         databaseURL: process.env.REACT_APP_databaseURL,
         projectId: process.env.REACT_APP_projectId,
         storageBucket: process.env.REACT_APP_storageBucket,
         messagingSenderId: process.env.REACT_APP_messagingSenderId,
         appId: process.env.REACT_APP_appId,
       };
-   
+
       const firebaseApp = firebase.initializeApp(firebaseConfig);
       this.db = firebaseApp.firestore();
     }
-    
+
     window.gapi.signin2.render('g-signin2', {
       'theme': 'dark',
       'onsuccess': this.onSuccess.bind(this),
-    });  
+    });
 
     this.selectProfile();
   }
@@ -77,18 +79,16 @@ class App extends Component {
   handleSignOut = () => {
     this.auth.signOut();
   };
-  
+
   handleLeavePage = (e) => {
     this.signOut();
     e.preventDefault();
     e.returnValue = true;
   }
-    
-  
-  
-  selectProfile(){
-    //randomly select players' profile
-    let ranNum = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  selectProfile() {
+    // randomly select players' profile
+    const ranNum = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     let ran1, ran2, ranTemp;
     for (let i = 0; i < 10; i++) {
       ran1 = Math.floor(Math.random() * 11);
@@ -119,104 +119,95 @@ class App extends Component {
     menuClass[i] = 'active';
     this.setState({ gameClass: menuClass });
   }
-  
+
+  isUserEqual(googleUser, firebaseUser) {
+    if (firebaseUser) {
+      var providerData = firebaseUser.providerData;
+      for (var i = 0; i < providerData.length; i++) {
+        if (providerData[i].providerId === firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
+        providerData[i].uid === googleUser.getBasicProfile().getId()) {
+        // We don't need to reauth the Firebase connection.
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
   onSuccess(googleUser) {
     if (this.state.isSignedIn) {
       this.signOut();
     } else {
-      
-      function isUserEqual(googleUser, firebaseUser) {
-        if (firebaseUser) {
-          var providerData = firebaseUser.providerData;
-          for (var i = 0; i < providerData.length; i++) {
-            if (providerData[i].providerId === firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
-                providerData[i].uid === googleUser.getBasicProfile().getId()) {
-              // We don't need to reauth the Firebase connection.
-              return true;
-            }
-          }
-        }
-        return false;
-      }
       // We need to register an Observer on Firebase Auth to make sure auth is initialized.
       firebase.auth().onAuthStateChanged((function(firebaseUser) {
         // Check if we are already signed-in Firebase with the correct user.
-        if (!isUserEqual(googleUser, firebaseUser)) {
+        if (!this.isUserEqual(googleUser, firebaseUser)) {
           // Build Firebase credential with the Google ID token.
-          var credential = firebase.auth.GoogleAuthProvider.credential(
-              googleUser.getAuthResponse().id_token);
+          var credential = firebase.auth.GoogleAuthProvider.credential(googleUser.getAuthResponse().id_token);
           // Sign in with credential from the Google user.
-          firebase.auth().signInWithCredential(credential).catch(function(error) {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            // The email of the user's account used.
-            var email = error.email;
-            // The firebase.auth.AuthCredential type that was used.
-            var credential = error.credential;
-            // ...
-          });
+          firebase.auth().signInWithCredential(credential);
         } else {
           this.uid = firebaseUser.uid;
           console.log('User already signed-in Firebase.');
         }
       }).bind(this));
-      let playerProfile =  googleUser.getBasicProfile();
-      let players = this.state.players;
-      players[0].img = <i className={players[0].name}><img src={playerProfile.getImageUrl()}></img></i>;
+      const playerProfile = googleUser.getBasicProfile();
+      const players = this.state.players;
+      players[0].img = (
+        <i className={players[0].name}>
+          <img src={playerProfile.getImageUrl()} alt="Player Profile" />
+        </i>
+      );
       players[0].name = playerProfile.getGivenName();
       this.googleUser = googleUser;
-      this.setState({players: players});
+      this.setState({ players: players });
     }
     this.setState({ isSignedIn: this.auth.isSignedIn.get() });
   }
 
   startNewLog() {
-    this.timestamp  =  Date.now();
+    this.timestamp = Date.now();
     var usersUpdate = {};
-    usersUpdate[`${this.timestamp}`] = {playerEmail: this.googleUser.getBasicProfile().getEmail(), playerId: this.googleUser.getBasicProfile().getGivenName(), versionNumber: "v1", totalPoints: 0};
-    this.db.collection("gameLogs").doc(this.uid).set(usersUpdate, {merge: true});
-
+    usersUpdate[`${this.timestamp}`] = { playerEmail: this.googleUser.getBasicProfile().getEmail(), playerId: this.googleUser.getBasicProfile().getGivenName(), versionNumber: 'v1', totalPoints: 0 };
+    this.db.collection('gameLogs').doc(this.uid).set(usersUpdate, { merge: true });
   }
 
   update(fieldAndvalue) {
     var usersUpdate = {};
     usersUpdate[`${this.timestamp}`] = fieldAndvalue;
-    this.db.collection("gameLogs").doc(this.uid).set(usersUpdate, {merge: true});
+    this.db.collection('gameLogs').doc(this.uid).set(usersUpdate, { merge: true });
   }
 
   signOut() {
-    this.auth.signOut().then(function () {
-    });
+    this.auth.signOut();
     this.auth.disconnect();
   }
 
   render() {
     return (
       <HashRouter basename = "/">
-        <div className="App" style={{ width: "100%", height: "100%", position:"relative"}} key="main">
-          <div className="header" >
+        <div className="App" style={{ width: '100%', height: '100%', position:'relative' }} key="main">
+          <div className="header">
             <div id="cmu">
-              <img src = {cmuLogo} alt="CMU logo" /></div>
-              
-              <Link to="/home/" className="title" onClick={(ev)=> {this.setState({ gameClass: [" ", " "]}); }}>Interpretable Machine Learning Research Project</Link>
-              <div className = "menuBar">
-            
-              <Link to="/guessai/" className={"menu " + this.state.gameClass[0]} key="menu0" onClick={(ev)=> {
-                this.setState({ gameClass: ["active", " "]}); 
-              }}>Guess AI</Link>
-              <div id="g-signin2" onClick={(env) => {this.signOut.bind(this)}}></div>
-              </div>
+              <img src = {cmuLogo} alt="CMU logo" />
             </div>
-          
-              <Route path ="/" exact render={props => <Home isSignedIn = {this.state.isSignedIn} setMenu = {this.setMenu.bind(this)} />} />
-              <Route path ="/home/" render={props => <Home isSignedIn = {this.state.isSignedIn} setMenu = {this.setMenu.bind(this)} />} />
-              <Route path = "/guessai/" render={props => <GAIHome  players = {this.state.players} startNewLog={this.startNewLog.bind(this)} update={this.update.bind(this)} setMenu = {this.setMenu.bind(this)}/>} />
-              <Route path = "/guessai-play/" render = {props => <GuessAI key = "guessAI" players = {this.state.players} update={this.update.bind(this)} setMenu = {this.setMenu.bind(this)} handleLeavePage = {this.handleLeavePage.bind(this)}/>}  />
-          
+            <Link
+              to="/"
+              className="title"
+              onClick={()=> { this.setState({ gameClass: [' ', ' '] }); }}
+            >
+              Interpretable Machine Learning Research Project
+            </Link>
+            <div className="menuBar">
+              <div id="g-signin2" onClick={() => { this.signOut.bind(this); }} />
+            </div>
+          </div>
+          <Route path ="/" exact render={() => <Landing isSignedIn = {this.state.isSignedIn} setMenu = {this.setMenu.bind(this)} />} />
+          <Route path = "/play" render = {() => <GuessAI key = "guessAI" players = {this.state.players} update={this.update.bind(this)} setMenu = {this.setMenu.bind(this)} handleLeavePage = {this.handleLeavePage.bind(this)}/>} />
         </div>
       </HashRouter>
     );
   }
 }
+
 export default App;
---
+++ frontend/src/Landing/Landing.js
@@ -0,0 +1,145 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import classnames from 'classnames';
+
+import styles from './Landing.module.scss';
+
+const vis = [];
+for (let i = 0; i < 10; i++) {
+  vis[i] = require('../image/mixed4d/' + (i + 1) + '.png');
+}
+
+class Home extends Component {
+  renderImgs() {
+    const element = [];
+    for (let i = 0; i < 10; i++) {
+      element.push(
+        <img
+          key={i}
+          src={vis[i]}
+          alt={`Visualization ${i + 1}`}
+        />
+      );
+    }
+    return element;
+  }
+
+  render() {
+    if (this.props.isSignedIn) {
+      return (
+        <div className={styles['home']}>
+          <div className={styles['home__wrapper']}>
+            <div className={styles['home__side']}>
+              <div className={styles['home--left']}>
+                {this.renderImgs()}
+              </div>
+            </div>
+            <div className={classnames(styles['home__side'], styles['home--right'])}>
+              <div className={styles['home__title']}>
+                Guess what the machine sees
+              </div>
+              <div className={styles['home__details']}>
+                <p>
+                  Research:&nbsp;
+                  <a href="https://distill.pub/2017/feature-visualization/" target="_blank" rel="noopener noreferrer">
+                    Feature visualization
+                  </a>
+                  &nbsp;is how neural networks build understanding of images.
+                  This is how AI interprets objects, patterns, animals, etc.
+                  Our goal is to understand how humans select and interpret feature visualizations.
+                </p>
+                <p>
+                  This study is part of a research project within the Visualization Group at Carnegie Mellon University.
+                </p>
+                <p>
+                  Try our game to learn more about feature visualization which help AI categorize images.
+                </p>
+                <p>
+                  Consent: Your decision to participate is voluntary and we thank you for advancing research.
+                  By playing this game, you agree that you are 18 or older and want to participate.
+                </p>
+                <p>
+                  Enjoy the game!
+                </p>
+              </div>
+              <div className={styles['home__links']}>
+                <Link
+                  className={styles['home__btn']}
+                  to="/guessai"
+                  onClick={() => { this.props.setMenu(0); }}
+                >
+                  How to Play
+                </Link>
+                <Link
+                  className={styles['home__btn']}
+                  to="/guessai-play"
+                  onClick={() => { this.props.setMenu(0); }}
+                >
+                  Play
+                </Link>
+              </div>
+            </div>
+          </div>
+          <p className={styles['home__footer']}>
+            Designed and developed at Carnegie Mellon University, © 2019
+          </p>
+        </div>
+      );
+    } else {
+      return (
+        <div className={styles['home']}>
+          <div className={styles['home__wrapper']}>
+            <div className={styles['home__side']}>
+              <div className={styles['home--left']}>
+                {this.renderImgs()}
+              </div>
+            </div>
+            <div className={classnames(styles['home__side'], styles['home--right'])}>
+              <div className={styles['home__title']}>
+                Guess what the machine sees
+              </div>
+              <div className={styles['home__details']}>
+                <p>
+                  Research:&nbsp;
+                  <a href="https://distill.pub/2017/feature-visualization/" target="_blank" rel="noopener noreferrer">
+                    Feature visualization
+                  </a>
+                  &nbsp;is how neural networks build understanding of images.
+                  This is how AI interprets objects, patterns, animals, etc.
+                  Our goal is to understand how humans select and interpret feature visualizations.
+                </p>
+                <p>
+                  This study is part of a research project within the Visualization Group at Carnegie Mellon University.
+                </p>
+                <p>
+                  Try our game to learn more about feature visualization which help AI categorize images.
+                </p>
+                <p>
+                  Consent: Your decision to participate is voluntary and we thank you for advancing research.
+                  By playing this game, you agree that you are 18 or older and want to participate.
+                </p>
+                <p>
+                  Enjoy the game!
+                </p>
+              </div>
+              <div className={styles['home__links']}>
+                <Link
+                  className={styles['home__btn']}
+                  to="/guessai"
+                  onClick={() => { this.props.setMenu(0); }}
+                >
+                  How to Play
+                </Link>
+              </div>
+            </div>
+          </div>
+          <p className={styles['home__footer']}>
+            Designed and developed at Carnegie Mellon University, © 2019
+          </p>
+        </div>
+      );
+    }
+  }
+}
+
+export default Home;
