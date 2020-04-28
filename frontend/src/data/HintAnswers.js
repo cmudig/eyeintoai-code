508752be6138269028e68609fcf4faa007a5a5ac
Kazi Jawad <kazi_Jawad@outlook.com>
2020-04-27 22:57:11 -0400

    Fix issue with inconsistent answer fields

--- frontend/src/data/HintAnswers.js
+++ frontend/src/data/HintAnswers.js
@@ -1,8 +1,8 @@
 export default {
-  'land animals': ['poodle', 'llama', 'snake', 'spider', 'cat', 'parrot', 'frog', 'bee', 'butterfly', 'tiger', 'bear', 'deer', 'mouse', 'hamster', 'wolf', 'fox'],
-  'sea animals': ['jellyfish', 'shark', 'turtle', 'starfish', 'dolphin', 'whale', 'octopus', 'turtle', 'squid', 'seal', 'seahorse', 'lobster', 'walrus', 'rays', 'crab', 'walrus'],
+  'land animal': ['poodle', 'llama', 'snake', 'spider', 'cat', 'parrot', 'frog', 'bee', 'butterfly', 'tiger', 'bear', 'deer', 'mouse', 'hamster', 'wolf', 'fox'],
+  'sea animal': ['jellyfish', 'shark', 'turtle', 'starfish', 'dolphin', 'whale', 'octopus', 'turtle', 'squid', 'seal', 'seahorse', 'lobster', 'walrus', 'rays', 'crab', 'walrus'],
   'fruit': ['strawberry', 'plum', 'pumpkin', 'banana', 'watermelon', 'orange', 'pineapple', 'apple', 'cherry', 'blueberry', 'grape', 'kiwi', 'lychee', 'plum', 'peach', 'pear'],
-  'vegetables': ['corn', 'broccoli', 'spinach', 'kale','asparagus', 'carrot', 'cucumber', 'potato', 'turnip', 'tomato', 'squash', 'pepper', 'okra', 'onion', 'leek', 'lettuce'],
+  'vegetable': ['corn', 'broccoli', 'spinach', 'kale','asparagus', 'carrot', 'cucumber', 'potato', 'turnip', 'tomato', 'squash', 'pepper', 'okra', 'onion', 'leek', 'lettuce'],
   'musical instrument': ['violin', 'piano', 'guitar', 'drums', 'flute', 'piano', 'ukulele', 'saxophone', 'cello', 'trumpet', 'oboe', 'xylophone', 'clarinet', 'tuba', 'banjo', 'bagpipes'],
   'transportation': ['train', 'car', 'plane', 'bike', 'taxi', 'ship', 'bus', 'truck', 'motorcycle', 'gondola', 'trolley', 'hoverboard', 'zipline', 'sled', 'sailboat', 'tricycle'],
   'electronics': ['camera', 'toaster', 'vacuum cleaner', 'laptop', 'fan', 'keyboard', 'phone', 'computer', 'headphones', 'xbox', 'lamp', 'blender', 'tablet', 'washing machine', 'dishwasher', 'speaker'],
